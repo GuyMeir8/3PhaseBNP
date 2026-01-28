da193a53946ceb81c12ba309a258209977977245
Guy Meir <megu@post.bgu.ac.il>
2026-01-28 18:22:53 +0200

    not tested, but all the geometry and mole calculations are written. BEFORE I let gemini do its thing

--- BNP_Gibbs_en_calc_3_phase.py
+++ BNP_Gibbs_en_calc_3_phase.py
@@ -139,12 +139,98 @@ class GibbsEnergyCalculator3Phase:
 
             return n_mp, x_mp
 
+        n_A_total = n_total * (1 - xB_total)
+        n_B_total = n_total * xB_total
+        
         if has_skin:
+            v_mp = self._get_v_mp(T, phases)
+            n_A_alpha = A_ratio_alpha * n_A_total
+            n_B_alpha = B_ratio_alpha * n_B_total
+            n_no_skin_mp, x_no_skin_mp = _calc_generic_split(n_A_alpha, n_B_alpha, n_A_total, n_B_total)
+            V_no_skin = np.sum(n_no_skin_mp * v_mp)
+            r_no_skin = self.calc_r_from_V(V_no_skin)
+            mats = self.system_data.config.materials
+            weighted_skin_thickness = 2 * (1 - xB_skin) * self.system_data.material_data[mats[0]].atomic_radius + 2 * xB_skin * self.system_data.material_data[mats[1]].atomic_radius
+            weighted_skin_molar_volume = (1 - xB_skin) * v_mp[0,2] + xB_skin * v_mp[1,2]
+            V_skin_prev_guess = (4/3) * np.pi * (r_no_skin ** 3 - (r_no_skin - weighted_skin_thickness) ** 3)
+            n_skin_prev_guess = V_skin_prev_guess / weighted_skin_molar_volume
+            
+            num_of_tries = 0
+            was_successful = False
+            while num_of_tries < 100:
+                num_of_tries += 1
+                n_A_skin = n_skin_prev_guess * (1 - xB_skin)
+                n_B_skin = n_skin_prev_guess * xB_skin
+                n_A_no_skin = n_A_total - n_A_skin
+                n_B_no_skin = n_B_total - n_B_skin
+                n_A_alpha = A_ratio_alpha * n_A_no_skin
+                n_B_alpha = B_ratio_alpha * n_B_no_skin 
+                n_mp_no_skin, x_mp_no_skin = _calc_generic_split(n_A_alpha, n_B_alpha, n_A_no_skin, n_B_no_skin)
 
-            pass # Placeholder logic once skin exists
+                match geometry_type:
+                    case "Janus":
+                        r_vals = self.calc_Janus_geometry_for_known_nx(
+                            n_mp_no_skin,
+                            x_mp_no_skin,
+                            phases,
+                            T,
+                        )
+                        h_a, h_b, a_i = r_vals
+                        r_a = (h_a**2 + a_i**2) / (2 * h_a)
+                        r_b = (h_b**2 + a_i**2) / (2 * h_b)
+                        V_no_skin = np.sum(n_no_skin_mp * v_mp[:,0:1])
+                        r_a_with_skin = r_a + weighted_skin_thickness
+                        r_b_with_skin = r_b + weighted_skin_thickness
+                        theta_a = np.arcsin(a_i / r_a)
+                        theta_b = np.arcsin(a_i / r_b)
+                        V_a_with_skin = (1/3) * np.pi * (r_a_with_skin**3) * (2 + np.cos(theta_a)) * (1 - np.cos(theta_a))**2
+                        V_b_with_skin = (1/3) * np.pi * (r_b_with_skin**3) * (2 + np.cos(theta_b)) * (1 - np.cos(theta_b))**2
+                        V_skin = V_a_with_skin + V_b_with_skin - V_no_skin
+                        n_skin_curr_guess = V_skin / weighted_skin_molar_volume
+                    case "Core_Shell":
+                        r_vals = self.calc_core_shell_geometry_for_known_nx(
+                            n_mp_no_skin,
+                            x_mp_no_skin,
+                            phases,
+                            T,
+                        )
+                        V_no_skin = np.sum(n_no_skin_mp * v_mp[:,0:1])
+                        r_with_skin = r_vals[1] + weighted_skin_thickness
+                        V_with_skin = (4/3) * np.pi * (r_with_skin ** 3)
+                        V_skin = V_with_skin - V_no_skin
+                        n_skin_curr_guess = V_skin / weighted_skin_molar_volume
+                    
+                if abs(n_skin_curr_guess - n_skin_prev_guess) < 1e-6:
+                    was_successful = True
+                    break
+                else:
+                    n_skin_prev_guess = n_skin_curr_guess*0.5 + n_skin_prev_guess*0.5
+
+            if not was_successful:
+                raise ValueError("Failed to converge skin mole calculation within 100 iterations.")
+            else:
+                n_A_skin = n_skin_curr_guess * (1 - xB_skin)
+                n_B_skin = n_skin_curr_guess * xB_skin
+
+                n_mp = np.array([
+                    [n_A_alpha, n_A_no_skin - n_A_alpha, n_A_skin],
+                    [n_B_alpha, n_B_no_skin - n_B_alpha, n_B_skin],
+                ])
+                n_alpha = np.sum(n_mp[:,0])
+                n_beta = np.sum(n_mp[:,1])
+                n_skin = np.sum(n_mp[:,2])
+                xA_alpha = n_A_alpha / n_alpha
+                xB_alpha = n_B_alpha / n_alpha
+                xA_beta = (n_A_no_skin - n_A_alpha) / n_beta
+                xB_beta = (n_B_no_skin - n_B_alpha) / n_beta
+                xA_skin = n_A_skin / n_skin
+                xB_skin = n_B_skin / n_skin
+                x_mp = np.array([
+                    [xA_alpha, xA_beta, xA_skin],
+                    [xB_alpha, xB_beta, xB_skin],
+                ])
+            
         else:
-            n_A_total = n_total * (1 - xB_total)
-            n_B_total = n_total * xB_total
             n_A_alpha = A_ratio_alpha * n_A_total
             n_B_alpha = B_ratio_alpha * n_B_total
             n_mp, x_mp = _calc_generic_split(n_A_alpha, n_B_alpha, n_A_total, n_B_total)
@@ -159,13 +245,12 @@ class GibbsEnergyCalculator3Phase:
                     )
 
                 case "Core_Shell":
-                    v_mp = self._get_v_mp(T, phases)
-                    V_alpha = np.sum(n_mp[:,0] * v_mp[:,0])
-                    V_total = np.sum(n_mp * v_mp)
-                    r_alpha = self.calc_r_from_V(V_alpha)
-                    r_total = self.calc_r_from_V(V_total)
-                    r_vals = np.array([r_alpha, r_total]) ### r_vals def - Core-Shell no Skin
-
+                    r_vals = self.calc_core_shell_geometry_for_known_nx(
+                        n_mp,
+                        x_mp,
+                        phases,
+                        T,
+                    )
         return n_mp, x_mp, r_vals  # Return the calculated mole splits and compositions
     
     def calc_Janus_geometry_for_known_nx(
@@ -232,7 +317,26 @@ class GibbsEnergyCalculator3Phase:
             sol = optimize.root(helper_Janus_geo_ri_calc, h_initial_guesses, method='hybr')
             term_inside_sqrt = (((6 * np.sum(nv_mp[:,0])) / (np.pi * sol.x[0])) - sol.x[0] ** 2) / 3
             a_i_result = (term_inside_sqrt ** (1/2))
-            r_vals = np.array([sol.x[0], sol.x[1], a_i_result]) ### r_vals def - Janus no Skin
+            h_a_res, h_b_res = sol.x
+            r_vals = np.array([h_a_res, h_b_res, a_i_result]) ### r_vals def - Janus no Skin
             return r_vals
         r_vals = helper_calc_Janus_geo(sigma_interface, sigma_alpha_out, sigma_beta_out)
+        return r_vals
+
+    def calc_core_shell_geometry_for_known_nx(
+            self,
+            n_mp: np.ndarray,
+            x_mp: np.ndarray,
+            phases: Tuple[str, ...],
+            T: float,
+    ) -> np.ndarray:
+        """
+        Calculates the geometry parameters for Core-Shell particles.
+        """
+        v_mp = self._get_v_mp(T, phases)
+        V_alpha = np.sum(n_mp[:,0] * v_mp[:,0])
+        V_total = np.sum(n_mp * v_mp)
+        r_alpha = self.calc_r_from_V(V_alpha)
+        r_total = self.calc_r_from_V(V_total)
+        r_vals = np.array([r_alpha, r_total]) ### r_vals def - Core-Shell no Skin
         return r_vals
